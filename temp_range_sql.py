0fd5049c9bf79b5ed64e996c5a7f24417130c1ca
Shirley940408 <Shirley940408@gmail.com>
2025-10-17 03:02:00 -0700

    add temp_range_sql.py

---
+++ temp_range_sql.py
@@ -0,0 +1,60 @@
+from pyspark.sql import SparkSession, types
+import sys
+
+# Explicit schema for GHCN daily files
+observation_schema = types.StructType([
+    types.StructField('station',     types.StringType()),
+    types.StructField('date',        types.StringType()),   # yyyymmdd
+    types.StructField('observation', types.StringType()),   # e.g., TMAX
+    types.StructField('value',       types.IntegerType()),  # tenths of °C
+    types.StructField('mflag',       types.StringType()),
+    types.StructField('qflag',       types.StringType()),   # quality flag
+    types.StructField('sflag',       types.StringType()),
+    types.StructField('obstime',     types.StringType()),
+])
+
+# add more functions as necessary
+def main(inputs, output):
+    spark = (SparkSession
+             .builder
+             .appName("temp_range_sql")
+             .getOrCreate())
+
+    spark.read.csv(inputs, schema=observation_schema).createOrReplaceTempView("weather")
+
+    spark.sql("""
+            SELECT station, date, observation, value
+            FROM weather
+            WHERE qflag IS NULL or qflag = ''
+            """).createOrReplaceTempView("good")
+
+    spark.sql("""
+            SELECT station, date, value AS tmax
+            FROM good 
+            WHERE observation = 'TMAX'
+            """).createOrReplaceTempView("tmax")
+
+    spark.sql("""
+            SELECT station, date, value AS tmin
+            FROM good
+            WHERE observation = 'TMIN'
+            """).createOrReplaceTempView("tmin")
+
+    spark.sql("""
+              SELECT tmax.date, tmax.tmax, tmax.station, ROUND((tmax.tmax - tmin.tmin)/10.0, 1) AS range
+              FROM tmax JOIN tmin ON tmax.station = tmin.station AND tmin.date = tmax.date
+              """).createOrReplaceTempView("range_weather")
+
+    result = spark.sql("""
+            SELECT station, date, range
+            FROM range_weather
+            ORDER BY range_weather.date, range_weather.station
+            """)
+
+    result.write.csv(output, mode='overwrite',header=True)
+
+if __name__ == '__main__':
+    inputs = sys.argv[1]
+    output = sys.argv[2]
+    assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
+    main(inputs, output)
