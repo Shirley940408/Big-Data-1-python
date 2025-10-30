ad834afbe6b06c5f6b875b73a0e8608769dd5a41
Shirley940408 <Shirley940408@gmail.com>
2025-10-30 14:07:15 -0700

    change typo of tuple to iterable object

--- load_logs.py
+++ load_logs.py
@@ -10,6 +10,8 @@
 # 205.212.115.106 - - [01/Jul/1995:00:00:12 -0400] "GET /shuttle/countdown/countdown.html HTTP/1.0" 200 3985
 # d104.aa.net - - [01/Jul/1995:00:00:13 -0400] "GET /shuttle/countdown/ HTTP/1.0" 200 3985
 # 129.94.144.152 - - [01/Jul/1995:00:00:13 -0400] "GET / HTTP/1.0" 200 7074
+# 用法:
+# python3 load_logs.py /home/bigdata/nasa-logs-2 sya236 nasalogs
 import gzip
 import os
 import re
@@ -24,7 +26,7 @@ def get_hostname_datatime_path_bytes(line):
     pattern = re.compile(r'^(\S+) - - \[(\S+) [+-]\d+\] \"[A-Z]+ (\S+) HTTP/\d\.\d\" \d+ (\d+)$')
     match = pattern.match(line)
     if match:
-        return tuple(match.group(i) for i in range (1,5)) # hostname, datetimeString, path, numbers_of_bytes_String
+        return (match.group(i) for i in range (1,5)) # hostname, datetimeString, path, numbers_of_bytes_String
 
 def main():
     input_dir = sys.argv[1]
@@ -46,7 +48,7 @@ def main():
                 hostname, datetimeString, path, numbers_of_bytes_String = get_hostname_datatime_path_bytes(line)
                 # format timeString to timestamp
                 naive = datetime.strptime(datetimeString, "%d/%b/%Y:%H:%M:%S")
-                bound_statement = prepared_statement.bind((hostname, naive, path, int(numbers_of_bytes_String), uuid.uuid1()))
+                bound_statement = prepared_statement.bind(tuple(hostname, naive, path, int(numbers_of_bytes_String), uuid.uuid1()))
                 batch.add(bound_statement)
                 count += 1
                 if count == BATCH_SIZE:
