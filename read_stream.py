8acaf359126f46fc8385f069671431fa809410f7
Shirley940408 <Shirley940408@gmail.com>
2025-11-13 15:51:46 -0800

    change code

--- read_stream.py
+++ read_stream.py
@@ -4,7 +4,7 @@ Sample Kafka consumer, to verify that messages are coming in on the topic we exp
 # spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 job.py
 import sys
 from kafka import KafkaConsumer
-# from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
 
 topic = sys.argv[1]
 consumer = KafkaConsumer(topic, bootstrap_servers=['node1.local:9092','node2.local:9092'],auto_offset_reset='latest')
@@ -13,8 +13,15 @@ for msg in consumer:
 
 # stream = streaming_df.….start()
 # stream.awaitTermination(600)
-# spark = SparkSession.builder.appName('read_stream').getOrCreate()
-# messages = spark.readStream.format('kafka') \
-#         .option('kafka.bootstrap.servers', 'node1.local:9092,node2.local:9092') \
-#         .option('subscribe', topic).load()
-# values = messages.select(messages['value'].cast('string'))
+spark = SparkSession.builder.appName('read_stream').getOrCreate()
+messages = spark.readStream.format('kafka') \
+        .option('kafka.bootstrap.servers', 'node1.local:9092,node2.local:9092') \
+        .option('subscribe', topic).load()
+values = messages.select(messages['value'].cast('string'))
+
+query = (values.writeStream
+         .format("console")
+         .outputMode("append")
+         .start())
+
+query.awaitTermination()
