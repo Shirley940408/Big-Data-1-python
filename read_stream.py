d215de262527190e9a0efb65b04bd4f47a6e6cc1
Shirley940408 <Shirley940408@gmail.com>
2025-11-13 15:18:09 -0800

    change typo

--- read_stream.py
+++ read_stream.py
@@ -5,6 +5,6 @@ import sys
 from kafka import KafkaConsumer
 
 topic = sys.argv[1]
-consumer = KafkaConsumer(topic, bootstrap_servers=['node1.local：9092'],auto_offset_reset='latest')
+consumer = KafkaConsumer(topic, bootstrap_servers=['node1.local:9092'],auto_offset_reset='latest')
 for msg in consumer:
     print(msg.value.decode('utf-8'))
