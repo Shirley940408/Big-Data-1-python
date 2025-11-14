538af28b5bfba1d8e0f0cccf708c7f5fec64be17
Shirley940408 <Shirley940408@gmail.com>
2025-11-13 16:05:38 -0800

    modify code

--- read_stream.py
+++ read_stream.py
@@ -10,23 +10,16 @@ topic = sys.argv[1]
 # consumer = KafkaConsumer(topic, bootstrap_servers=['node1.local:9092','node2.local:9092'],auto_offset_reset='latest')
 # for msg in consumer:
 #     print(msg.value.decode('utf-8'))
-
-# stream = streaming_df.….start()
-# stream.awaitTermination(600)
 spark = SparkSession.builder.appName('read_stream').getOrCreate()
 messages = spark.readStream.format('kafka') \
         .option('kafka.bootstrap.servers', 'node1.local:9092,node2.local:9092') \
         .option('subscribe', topic).load()
+
 values = messages.select(messages['value'].cast('string'))
-query = (values.writeStream
+
+stream = (values.writeStream
          .format("console")
          .outputMode("append")
          .start())
 
-
-query = (values.writeStream
-         .format("console")
-         .outputMode("append")
-         .start())
-
-query.awaitTermination()
+stream.awaitTermination()
